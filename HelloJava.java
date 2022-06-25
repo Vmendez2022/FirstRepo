b801905a608bb10f8cf503e5f033b5ce292d163e
Victor Mendez <victorhugoone@gmail.com>
2022-06-24 20:33:58 -0500

    My first commit

--- HelloJava.java
+++ HelloJava.java
@@ -1,6 +1,8 @@
 public class HelloJava {
 	
 	public static void main(String[] args) {
+             
+                 String = Hello "Estoy tomando el curso de Tester Deovs";                
 
 		System.out.println("Hello Java");
 		  
